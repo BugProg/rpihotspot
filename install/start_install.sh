5c89cbded4a3f4dae6734454d7f2a0ef43f0594f
Tifiloow <theophilejeromerocher44@gmail.com>
2019-11-14 15:22:08 +0100

    Update start_install.sh

--- install/start_install.sh
+++ install/start_install.sh
@@ -1,5 +1,5 @@
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt install dnsmasq hostapd
 sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
@@ -10,32 +10,33 @@ else
     sudo apt-get install python3.6
 fi
 
+npm install body-parser mongoose bcryptjs express jsonwebtoken
 
-install.py dhcpcd
+sudo install.py dhcpcd
 
 sudo service dhcpcd restart
 
 
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-install.py dnsmasq 
+sudo install.py dnsmasq 
 
 
 sudo systemctl start dnsmasq
 sudo systemctl reload dnsmasq
 
-install.py hostapdconf
+sudo install.py hostapdconf
 
 
-install.py daemonhostapd
+sudo install.py daemonhostapd
 
 
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 sudo systemctl start hostapd
 
-install.py forward
+sudo install.py forward
 sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
-install.py restore
+sudo install.py restore
 #https://www.raspberrypi.org/documentation/configuration/wireless/access-point.md
